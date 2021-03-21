5a0b0d3bcffed229ee82097ac8f3978904856b6e
Diana Toth <dianatoth.dtt@gmail.com>
2021-03-21 16:38:34 +0200

    input validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,14 +3,19 @@ import { Router } from "@angular/router"
 import { AuthService } from "./auth.service"
 
 @Component({
-    templateUrl:'./login.component.html'
+    templateUrl:'./login.component.html',
+    styles:[`
+    em{float:right; color:red; padding-left:10px;}
+    `]
 })
 export class LoginComponent{
     constructor(private authService:AuthService, private router:Router){
 
     }
-    // username
-    // password
+    username
+    password
+    mouseoverLogin
+    
      login(formValues){
          console.log(formValues)
          this.authService.loginUser(formValues.userName, formValues.password)
