781f782451413cd372a89b30336bb6a5b51a9566
Diana Toth <dianatoth.dtt@gmail.com>
2021-03-21 13:37:52 +0200

    user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { RouterModule } from '@angular/router';
 import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import {NavBarComponent} from './nav/navbar.component'
-import {EventService} from './events/shared/event.service'
+import { NavBarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
-import {appRoutes} from '../routes'
+import { appRoutes } from '../routes'
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.component';
