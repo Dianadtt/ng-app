35f4cbba458d1b0711b96c876bf8093d348045f7
Diana Toth <dianatoth.dtt@gmail.com>
2021-03-21 10:34:24 +0200

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import {NavBarComponent} from './nav/navbar.component'
 import {EventService} from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import {appRoutes} from '../routes'
+
+
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent,
+
     
   ], 
   providers:[EventService, ToastrService],
